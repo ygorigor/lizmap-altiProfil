6165cd4979ffab7ba97a7173dcbc5b56edb51c3b
ygorigor <sorocean.igor@gmail.com>
2024-09-05 12:30:50 +0300

    some modifications

--- altiProfil/install/www/altiprofil/js/altiProfil.js
+++ altiProfil/install/www/altiprofil/js/altiProfil.js
@@ -1,5 +1,6 @@
+/*jshint esversion: 6 */
 lizMap.events.on({
-    'uicreated': function(e) {
+    uicreated: (e) => {
         $('#profil-stop').click(function(){
             $('#button-altiProfil').click();
         });
@@ -16,8 +17,8 @@ function getAltiJsonResponse(params, aCallback){
             if(aCallback){
                     aCallback(JSON.parse(data));
             }
-        }
-        ,'json'
+        },
+        'json'
     );
 }
 
@@ -26,26 +27,31 @@ function getAlti(lon,lat, numFeat){
     if(lizMap.map.projection.projCode != "EPSG:4326"){
         var fromProjection = new OpenLayers.Projection(lizMap.map.projection.projCode);
         var toProjection = new OpenLayers.Projection("EPSG:4326");
+        var Point = new OpenLayers.LonLat(lon, lat);
         var convertedPoint = new OpenLayers.LonLat(lon, lat);
         convertedPoint.transform(fromProjection, toProjection);
         lon = convertedPoint.lon;
-        lat = convertedPoint.lat;    }
+        lat = convertedPoint.lat;}
 
     var qParams = {
-        'lon': lon,
+        'lon':lon,
         'lat':lat,
         'srs': lizMap.map.projection.projCode,
         'repository': lizUrls.params.repository,
         'project': lizUrls.params.project
     }
     getAltiJsonResponse(qParams, function(data){
-        var alt = data['elevations'][0]['z'];
-        $('#altiProfil .menu-content #alt-p'+numFeat).html( alt );
+        var alt = data.elevations[0].z;
+        $('#altiProfil .menu-content #alt-p'+numFeat).html( alt ).append(' m');
     });
+    $('#altiProfil .menu-content #altiProfil_help_p1').hide();
+    $('#altiProfil .menu-content #altiProfil_help_p2').show();
 }
 
 function getProfilJsonResponse(params, aCallback){
-    $('#altiProfil .menu-content #profil-chart .spinner').show();
+    $('#altiProfil .menu-content #altiProfil_help_p1').hide();
+    $('#altiProfil .menu-content #altiProfil_help_p2').hide();
+    $('#altiProfil .menu-content #profil-chart .altiProfil-spinner').show();
     $.get(
         URLAJAXALTIPROFIL,
         params,
@@ -53,19 +59,13 @@ function getProfilJsonResponse(params, aCallback){
             if(aCallback){
                     aCallback(JSON.parse(data));
             }
-        }
-        ,'json'
+        },
+        'json'
     );
 }
 
-function resizePlot(id){
-    var d3 = Plotly.d3;
-    var gd = d3.select('#'+id)
-    .style({
-        width: '100%',
-        margin: '0px'
-    });
-    Plotly.Plots.resize(gd.node());
+function resizePlot(){
+    window.dispatchEvent(new Event('resize'));
 }
 
 function getProfil(p1,p2){
@@ -88,14 +88,28 @@ function getProfil(p1,p2){
         'project': lizUrls.params.project,
         'sampling' : Math.round(p1.distanceTo(p2)/25) /* Only use with french mapping Agency (IGN) web service  */,
         'distance' : Math.round(p1.distanceTo(p2))
+    };
+    function choose_coordsunits(lat, lon) {
+        var coordsunits = '';
+        var position = '';
+        if ($('#content').hasClass('mobile')) {
+            coordsunits = 'dms';
+        }else{
+            coordsunits = $('lizmap-mouse-position > div.coords-unit > select').val();
+        }
+        if ((coordsunits === 'dm') || (coordsunits === 'dms')) {
+            position = OpenLayers.Util.getFormattedLonLat(lon, 'lon', coordsunits)+' / '+ OpenLayers.Util.getFormattedLonLat(lat, 'lat', coordsunits);
+        }else if ((coordsunits === 'degrees') || (coordsunits === 'm')) {
+                position = lon.toFixed(5)+' / '+ lat.toFixed(5);
+        }
+    return position;
     }
-
     getProfilJsonResponse(qParams, function(data){
-        var _x = data[0]['x'];
-        var _y = data[0]['y'];
-        var _customdata = data[0]['customdata'];
-        var _srs = data[0]['srid'];
-        var _altisource = data[0]['altisource'];
+        var _x = data[0].x;
+        var _y = data[0].y;
+        var _customdata = data[0].customdata;
+        var _srs = data[0].srid;
+        var _altisource = data[0].altisource;
 
         var layout = {
             title: '<b>'+LOCALES_ALTI_PROFIL+'</b>',
@@ -109,15 +123,16 @@ function getProfil(p1,p2){
             hovermode:'closest',
             annotations: [{
                 font: {
-                    size: 11
+                    size: 12
                 },
                 align:'center',
                 xref:'paper',
                 yref:'paper',
                 y: 1.16,
                 showarrow: false,
-                text: `point 1 (${Math.round(p1.x)},${Math.round(p1.y)}) | point 2 (${Math.round(p2.x)},${Math.round(p2.y)})`
-            },{
+                //text: `point 1 (${Math.round(p1.x)},${Math.round(p1.y)}) | point 2 (${Math.round(p2.x)},${Math.round(p2.y)})`
+            text: `P1 (${choose_coordsunits(p1Point.lat, p1Point.lon)}) | P2 (${choose_coordsunits(p2Point.lat, p2Point.lon)})<br>${LOCALES_ALTI_ALTITUDE} P1: ${_y[0]}m | ${LOCALES_ALTI_ALTITUDE} P2: ${_y[_y.length - 1]}m`
+            }/*,{
                 font: {
                     size: 10
                 },
@@ -128,19 +143,24 @@ function getProfil(p1,p2){
                 y: -0.21,
                 showarrow: false,
                 text: `<i>${LOCALES_ALTI_DATASOURCE} : ${_altisource}</i>`
-            }],
+            }*/],
             showlegend: false,
-            autosize: true
+            autosize: true,
+            margin: {
+                l: 60,
+                r: 10,
+                b: 60
+            }
         };
 
         //add extra info if datasource from DB
         if ( ALTI_PROVIDER == "database"){
-            var _resolution = data[0]['resolution'];
-            var _slope = data[0]['slope'];
-            _slope = $.parseJSON(_slope);
+            var _resolution = data[0].resolution;
+           // var _slope = data[0]['slope'];
+           // _slope = $.parseJSON(_slope);
 
-            layout['title'] = '<b>Profil ('+ LOCALES_ALTI_RESOLUTION +' ' +_resolution+ 'm)';
-            layout['annotations'].push(
+            layout.title = '<b>Profil ('+ LOCALES_ALTI_RESOLUTION +' ' +_resolution+ 'm)';
+            /*layout['annotations'].push(
                 {
                     font: {
                         size: 11
@@ -152,7 +172,7 @@ function getProfil(p1,p2){
                     showarrow:false,
                     text: `${LOCALES_ALTI_SLOPE} ${LOCALES_ALTI_UNIT}  min :  ${_slope.min_slope} | max : ${_slope.max_slope} | ${LOCALES_ALTI_MEAN} : ${_slope.mean_slope}`
                 }
-            )
+            )*/
         }
 
         var profilLine = {
@@ -163,19 +183,33 @@ function getProfil(p1,p2){
             line: {
               color: 'rgb(128, 0, 128)',
               width: 1
-            }
-            ,hovertemplate: '<b>Altitude</b>: %{y}' +
-            '<br /><b>lon</b> : %{customdata[0].lon:.2f} / <b>lat</b> : %{customdata[0].lat:.2f}</b>'+
+            },
+            hovertemplate: '<b>' + LOCALES_ALTI_ALTITUDE + '</b>: %{y}m' +
+            '<br /><b>' + LOCALES_ALTI_DISTANCE + '</b> : %{x}m'+
+            '<br /><b>lon</b> : %{customdata[0].lon:.4f} / <b>lat</b> : %{customdata[0].lat:.4f}</b>'+
             '<extra></extra>'
           };
-        var data = [profilLine];
+          var StartStopLine = {
+            x: [_x[0], _x[_x.length - 1]],
+            y: [_y[0], _y[_y.length - 1]],
+            customdata:_customdata,
+            mode: 'lines+markers',
+            line: {
+              color: 'red',
+              width: 1.5
+            },
+            hovertemplate: '<b>' + LOCALES_ALTI_ALTITUDE + '</b>: %{y}m' +
+            '<br /><b>' + LOCALES_ALTI_DISTANCE + '</b> : %{x}m'+
+            '<extra></extra>'
+          };
+        data = [profilLine,StartStopLine];
 
         var plotLocale = navigator.language || navigator.userLanguage;
         var config = {
             showlegend: false,
             displaylogo: false,
-            responsive: true,
             locale: plotLocale,
+            responsive: true,
             toImageButtonOptions: {
               format: 'jpeg', // one of png, svg, jpeg, webp
               filename: 'profil',
@@ -183,12 +217,13 @@ function getProfil(p1,p2){
               width: 700,
               scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
             },
-            modeBarButtonsToRemove: ['zoom2d', 'pan2d','select2d','lasso2d','resetScale2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d',
+           /* modeBarButtonsToRemove: ['zoom2d', 'pan2d','select2d','lasso2d','resetScale2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d',
                 'resetScale2d', 'hoverClosestGl2d', 'hoverClosestPie', 'toggleHover', 'resetViews',
-                'sendDataToCloud', 'toggleSpikelines', 'resetViewMapbox', 'hoverClosestCartesian', 'hoverCompareCartesian']
+                'sendDataToCloud', 'toggleSpikelines', 'resetViewMapbox', 'hoverClosestCartesian', 'hoverCompareCartesian']*/
           };
         Plotly.newPlot('profil-chart-container', data, layout, config);
-        $('#altiProfil .menu-content #profil-chart .spinner').hide();
+        $('#altiProfil .menu-content #profil-chart .altiProfil-spinner').hide();
+        $('#altiProfil-box').css({'width': '500px'});
         var myPlot = document.getElementById('profil-chart-container');
 
         myPlot.on('plotly_click', function(data){            
@@ -208,7 +243,7 @@ function getProfil(p1,p2){
             ]);
         });
         document.getElementsByClassName('xtitle')[0].y.baseVal[0].value = document.getElementsByClassName('xtitle')[0].y.baseVal[0].value - 20;
-        resizePlot('profil-chart-container')
+        resizePlot();
     });
 }
 
@@ -262,8 +297,9 @@ function initAltiProfil() {
             if(altiProfilLayer.features.length>=2){
                 altiProfilLayer.destroyFeatures();
                 $('#altiProfil .menu-content #profil-chart').hide();
+                $('#altiProfil-box').css({'width': ''});
                 $('#altiProfil .menu-content #profil-chart-container').empty();
-                $('#altiProfil .menu-content span').html( "..." );
+                $('#altiProfil .menu-content #profil-chart-container').removeClass('js-plotly-plot');
             }
             var lonlat = map.getLonLatFromPixel(e.xy);
             altiProfilLayer.addFeatures([
@@ -301,6 +337,9 @@ function initAltiProfil() {
                 ctrl.deactivate();
             }
         });
+        $('#altiProfil-box').css({'width': ''});
+        $('#altiProfil .menu-content #altiProfil_help_p1').show();
+        $('#altiProfil .menu-content #altiProfil_help_p2').hide();
         altiProfilLayer.setVisibility(true);
         profilClick.activate();
     }
@@ -316,42 +355,45 @@ function initAltiProfil() {
                 ctrl.activate();
             }
         });
+        $('#altiProfil .menu-content #profil-chart').hide();
         $('#altiProfil .menu-content #profil-chart-container').empty();
-        $('#altiProfil .menu-content span').html( "..." );
+        $('#altiProfil .menu-content #profil-chart-container').removeClass('js-plotly-plot');
         altiProfilLayer.destroyFeatures();
         altiProfilLayer.setVisibility(false);
         profilClick.deactivate();
     }
+    $('#altiProfil-stop').click(function(){$('#button-altiProfil').click();});
+    $('#altiProfil-stop').tooltip({placement: 'bottom'});
 
     lizMap.events.on({
         // Dock opened
-        dockopened: function(e) {
+        dockopened: (e) => {
             if ( e.id == 'altiProfil' ) {
                 onAltiDockOpened();
             }
         },
-        minidockopened: function(e) {
+        minidockopened: (e) => {
             if ( e.id == 'altiProfil' ) {
                 onAltiDockOpened();
             }
         },
-        rightdockopened: function(e) {
+        rightdockopened: (e) => {
             if ( e.id == 'altiProfil' ) {
                 onAltiDockOpened();
             }
         },
         // Dock closed
-        dockclosed: function(e) {
+        dockclosed: (e) => {
             if ( e.id == 'altiProfil' ) {
                 onAltiDockClosed();
             }
         },
-        minidockclosed: function(e) {
+        minidockclosed: (e) => {
             if ( e.id == 'altiProfil' ) {
                 onAltiDockClosed();
             }
         },
-        rightdockclosed: function(e) {
+        rightdockclosed: (e) => {
             if ( e.id == 'altiProfil' ) {
                 onAltiDockClosed();
             }
--- altiProfil/templates/altiProfil_Dock.tpl
+++ altiProfil/templates/altiProfil_Dock.tpl
@@ -7,6 +7,7 @@
     var LOCALES_ALTI_ELEVATION = "{@altiProfil~altiProfil.alti.elevation@}";
     var LOCALES_ALTI_RESOLUTION = "{@altiProfil~altiProfil.alti.resolution@}";
     var LOCALES_ALTI_SLOPE = "{@altiProfil~altiProfil.alti.slope@}";
+    var LOCALES_ALTI_ALTITUDE = "{@altiProfil~altiProfil.alti.altitude@}";
     
     var ALTI_PROVIDER = "{$altiProvider}";
 
@@ -21,16 +22,22 @@
     var LOCALES_ALTI_DATASOURCE = "{@altiProfil~altiProfil.alti.datasource@}";  
   
 </script>
-<div id="altiProfil">
+<div class="altiProfil">
+  <h3>
+    <span class="title">
+      <button id= "altiProfil-stop" class="btn-altiProfil btn btn-mini btn-error btn-link" title="{@view~map.toolbar.content.stop@}">×</button>
+      <span class="icon"></span>
+      <span class="text">&nbsp;{@altiProfil~altiProfil.dock.title@}&nbsp;</span>
+    </span>
+  </h3>
   <div class="menu-content">
-    {@altiProfil~altiProfil.dock.help@} 
-    <p>
-      {@altiProfil~altiProfil.alti.point1@} : <span id="alt-p1"></span> <br/>
-      {@altiProfil~altiProfil.alti.point2@} : <span id="alt-p2"></span>
-    </p>
+  <div id="altiProfil-box">
+    <div id="altiProfil_help_p1">{@altiProfil~altiProfil.dock.help.p1@}</div>
+    <div id="altiProfil_help_p2">{@altiProfil~altiProfil.dock.help.p2@}</div>
     <div id="profil-chart">
-      <div class="spinner"></div>
+      <div class="altiProfil-spinner"></div>
       <div id="profil-chart-container"></div>
     </div>  
   </div>
+</div>
 </div>
